54c145ad8bf5e030273ba94ade63c46f0ded4b63
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-13 11:28:43 -0700

    initialize project

---
+++ planner_project/src/TodoList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() !== '') {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: inputText,
+      };
+      setTodos([...todos, newTodo]);
+      setInputText('');
+    }
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
