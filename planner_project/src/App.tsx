0f462fd49dce97ff77ba104fdd5d0528f7faab46
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-17 09:32:57 -0700

    incomplete, work elsewhere

--- planner_project/src/App.tsx
+++ planner_project/src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-
 import './App.css'
-
 import TodoList from './TodoList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      
       <TodoList />
+  
     </>
   )
 }
---
+++ planner_project/src/NavBar.tsx
@@ -0,0 +1,15 @@
+import { useState} from "react";
+
+const NavBar: React.FC = () =>{
+    return(
+        <div className="navbar">
+            
+        </div>
+    )
+
+
+
+}
+
+
+export default NavBar;
