908fa7873d6bda42ac13d580e9887642c7d1f230
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-17 22:58:12 -0700

    just copied the useFetch from tutorial lmao

--- planner_project/src/NavBar.tsx
+++ planner_project/src/NavBar.tsx
@@ -3,8 +3,13 @@ import { useState} from "react";
 const NavBar: React.FC = () =>{
     return(
         <div className="navbar">
-            
-        </div>
+            <div className="left-section">
+                <button className="sharp-edge">Game</button>
+                <button className="sharp-edge">Stats</button>
+            </div>
+        <button className="smooth-edge">Profile</button> 
+        {/*make this a component by itself, make menu to look at name, list of groups, include setting button*/ }
+    </div>
     )
 
 
---
+++ planner_project/src/useFetch.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+
+interface FetchResult<T> {
+  data: T | null;
+  error: string| null;
+  loading: boolean;
+}
+
+
+const useFetch = <T,>(url: string):FetchResult<T> => { //how the fuck do I make generics work here???
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => {
+      if (!res.ok) { // error coming back from server
+        throw Error(`Request error with code ${res.status}`);
+      } 
+      return res.json();
+    })
+    .then(data => {
+      setLoading(false);
+      setData(data);
+      setError(null);
+    })
+    .catch(err => {
+      // auto catches network / connection error
+      setLoading(false);
+      setError(err.message);
+    })
+}, [url]);
+
+  return { data, error, loading };
+}
+
+export default useFetch;
+
