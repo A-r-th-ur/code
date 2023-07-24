e2e80615f153c71122f66f7221e9f065c456eacc
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-23 18:47:34 -0700

    initial project creation

---
+++ journal_automator/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import FormatInput from "./lib/FormatInput.svelte";
+
+  
+  
+</script>
+
+<main>
+  <FormatInput />
+  
+</main>
+
+<style>
+  
+</style>
---
+++ journal_automator/src/lib/FormatInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    let input;
+    const handleInput = (evt) => {
+        input = evt.target.value;
+    }
+    
+
+</script>
+
+<form>
+    <input type = "text" placeholder="Write Something" on:input = {handleInput}/>
+</form>
