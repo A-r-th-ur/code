af9e4d4ea9fdbaaef88095f20fd126b9b463252e
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-25 00:59:51 -0700

    trying to fix the typing of svelte:component

--- journal_automator/src/App.svelte
+++ journal_automator/src/App.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
-    import BaseTextProcessor from "./lib/BaseTextProcessor.svelte";
+    //this file is the initiator of all states in the app
     import TextBox from "./lib/TextBox.svelte";
 
+    //import all content types
+    
+    
+    //handle text input
     let baseText: string;  
     const baseTextSetter = (evt: any) => {
     baseText = evt.target.value;
+  
+    //handle translation to dynamic component list
+    import type SvelteComponent from 'svelte';
+    let listOfContent: any[]; //should be type of sveltecomponent but typescript is not happy about it
+    const addToContentList = (content: any/*typeof SvelteComponen*/) => {
+      listOfContent.push(content);
+    }
+
+
+
   }
   
 </script>
 
 <main>
   <TextBox handleInput = {baseTextSetter}/>
-  <BaseTextProcessor {baseText}/>
+    {#each listOfContent as ,}
+    {/each}
 
   
 </main>
