e513dd7920b6f9aade0a276164d3f14513d2d52a
Arthur <ruhtra.zhang@mail.utoronto.ca>
2023-07-25 15:39:18 -0700

    now encountering the bug about each block  doesn't recognize the fucking list

--- journal_automator/src/App.svelte
+++ journal_automator/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     //this file is the initiator of all states in the app
     import TextBox from "./lib/TextBox.svelte";
-
+    import type { SvelteComponent } from 'svelte'; //must import at the beginning, otherwise devastating bug due to shitty compile time error
+    import Date from "./lib/content_types/Date.svelte";
+    import Duration from "./lib/content_types/Duration.svelte";
     //import all content types
     
     
@@ -11,9 +13,23 @@
     baseText = evt.target.value;
   
     //handle translation to dynamic component list
-    import type SvelteComponent from 'svelte';
-    let listOfContent: any[]; //should be type of sveltecomponent but typescript is not happy about it
-    const addToContentList = (content: any/*typeof SvelteComponen*/) => {
+     /*this is fucking stupid holy shit it only adds the end tag when I start to do it lmao
+        /week is week#
+        /date is date, default current date
+        /time is time value, default current time
+        <text> is for text with default value, 
+        /duration is for period of time, default 7.75
+        [persist] is for stored text with default value, and uses last time input for default value if not first time using
+        
+        
+    */
+    //Problem is that I have to dynamically generate all the components, and I don't think that anything in the template can be truly dynamic. 
+    //current plan is: first dynamically generate a list of components... this would be easier in react.
+    let listOfContent: typeof SvelteComponent[] = [
+      Date,
+      Duration
+    ]; //should be type of sveltecomponent but typescript is not happy about it
+    const addToContentList = (content: typeof SvelteComponent) => {
       listOfContent.push(content);
     }
 
@@ -25,8 +41,10 @@
 
 <main>
   <TextBox handleInput = {baseTextSetter}/>
-    {#each listOfContent as ,}
-    {/each}
+
+  {#each listOfContent as item}
+  {:else}
+  {/each};
 
   
 </main>
